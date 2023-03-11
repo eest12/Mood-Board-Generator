fd347a77fa3b08dfed34410abca31e685b70ac1f
Erika Estrada <edem1228@gmail.com>
2023-03-10 16:49:44 -0800

    Set up a page that calls the Type.fit and Unsplash APIs to fetch quotes and images. A random quote is displayed on top of an image or color block, depending on the selected option. There are controls to fetch a new quote, fetch a new image or change the color, and to change the background to be either an image or a color.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import { unsplashKey } from "./config";
+import { useEffect, useReducer, useState } from "react";
+
+function getRandomIndex(data) {
+  if (data && data.length > 0) {
+    return Math.floor(Math.random() * data.length);
+  }
+  return -1;
+}
+
+function getRandomColor() {
+  const rgb = [];
+  for (let i = 0; i < 3; i++) {
+    rgb.push(Math.floor(Math.random() * 256));
+  }
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+}
+
+function fetchImage(setImage, setError) {
+  fetch(`https://api.unsplash.com/photos/random?client_id=${unsplashKey}`)
+    .then((res) => res.json())
+    .then(setImage)
+}
+
+function Quote({ quotes, quoteIndex }) {
+  if (quotes != null && quotes[quoteIndex] != null) {
+    return (
+      <figure className="Quote Center">
+        <blockquote className="Quote-text">{quotes[quoteIndex].text}</blockquote>
+        <figcaption><i>&#8212;{quotes[quoteIndex].author == null ? "Unknown" : quotes[quoteIndex].author}</i></figcaption>
+      </figure>
+    );
+  }
+
+  return <p>Something went wrong. Try again.</p>;
+}
+
+function QuoteBlock({ quoteData, imageData, color, setColor }) {
+  if (imageData) {
+    return (
+      <div className="Quote-block" style={{ backgroundImage: `url(${imageData.urls.small}`, backgroundSize: "cover" }}>
+        <Quote quotes={quoteData.quotes} quoteIndex={quoteData.quoteIndex} />
+      </div>
+    );
+  }
+  if (color && setColor) {
+    return (
+      <div className="Quote-block" style={{ backgroundColor: color }}>
+        <Quote quotes={quoteData.quotes} quoteIndex={quoteData.quoteIndex} />
+      </div>
+    );
+  }
+  return (
+    <div className="Quote-block">
+      <Quote quotes={quoteData.quotes} quoteIndex={quoteData.quoteIndex} />
+    </div>
+  );
+}
 
 function App() {
+  const [quoteData, setQuoteData] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { quotes: null, quoteIndex: 0 }
+  )
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(null);
+  const [color, setColor] = useState(getRandomColor());
+  const [imgAsBackground, setImgAsBackground] = useState(true);
+
+  useEffect(() => {
+    console.log("here");
+    setLoading(true);
+    setTimeout(() => {
+      fetch("https://type.fit/api/quotes")
+        .then((res) => res.json())
+        .then((res) => setQuoteData({ quotes: res, quoteIndex: getRandomIndex(res) }))
+        .then(() => setLoading(false))
+        .catch((err) => setError(err));
+    }, "2000");
+  }, []);
+
+  useEffect(() => {
+    fetchImage(setImage);
+  }, [])
+
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Mood Board Generator</p>
       </header>
+      <div className="Main-content">
+        {loading ? <p>Loading...</p> : <QuoteBlock quoteData={quoteData} imageData={imgAsBackground ? image : null} color={!imgAsBackground ? color : null} setColor={!imgAsBackground ? setColor : null} />}
+
+        <div className="Buttons">
+          <button className="Button" onClick={() => { setQuoteData({ quoteIndex: getRandomIndex(quoteData.quotes) }) }}>New quote</button>
+          <button className="Button" onClick={() => imgAsBackground ? fetchImage(setImage) : setColor(getRandomColor())}>New {imgAsBackground ? "image" : "color"}</button>
+        </div>
+
+        <div className="Radio-buttons">
+          <input type="radio" id="image" name="background_type" value="Image" onChange={() => setImgAsBackground(true)} checked={imgAsBackground} />
+          <label htmlFor="image">Image</label>
+          <input type="radio" id="color" name="background_type" value="Color" onChange={() => setImgAsBackground(false)} checked={!imgAsBackground} />
+          <label htmlFor="color">Color</label>
+        </div>
+      </div>
     </div>
   );
 }
